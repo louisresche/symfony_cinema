a6a80cdfe4628e03a62e930d76727dbd9a04b7f5
louisresche <louis.resche@gmail.com>
2017-02-01 11:40:08 +0100

    add db + entity

--- symfony_cinema/src/symfony_cinema/CinemaBundle/Controller/DefaultController.php
+++ symfony_cinema/src/symfony_cinema/CinemaBundle/Controller/DefaultController.php
@@ -1,5 +1,5 @@
 <?php
-
+// src/symfony_cinema/CinemaBundle/Controller/DefaultController.php
 namespace symfony_cinema\CinemaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,5 +13,24 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('symfony_cinemaCinemaBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/Films")
+     */
+    public function listAction()
+    {
+        $films = $this->getDoctrine()->getRepository('symfony_cinemaCinemaBundle:Film')->findAll();
+
+
+        return $this->render('symfony_cinemaCinemaBundle:Film:list.html.twig');
+    }
+
+    /**
+     * @Route("/Film/{id}", requirements={"id": "\d+"})
+     */
+    public function showAction($id)
+    {
+        return $this->render('symfony_cinemaCinemaBundle:Film:show.html.twig');
     }
 }
---
+++ symfony_cinema/src/symfony_cinema/CinemaBundle/Entity/Film.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace symfony_cinema\CinemaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Film
+ *
+ * @ORM\Table(name="film")
+ * @ORM\Entity(repositoryClass="symfony_cinema\CinemaBundle\Repository\FilmRepository")
+ */
+class Film
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="synopsis", type="text")
+     */
+    private $synopsis;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_de_sortie", type="datetime")
+     */
+    private $dateDeSortie;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="realisateur", type="string", length=255)
+     */
+    private $realisateur;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="genre", type="string", length=255)
+     */
+    private $genre;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Film
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set synopsis
+     *
+     * @param string $synopsis
+     *
+     * @return Film
+     */
+    public function setSynopsis($synopsis)
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    /**
+     * Get synopsis
+     *
+     * @return string
+     */
+    public function getSynopsis()
+    {
+        return $this->synopsis;
+    }
+
+    /**
+     * Set dateDeSortie
+     *
+     * @param \DateTime $dateDeSortie
+     *
+     * @return Film
+     */
+    public function setDateDeSortie($dateDeSortie)
+    {
+        $this->dateDeSortie = $dateDeSortie;
+
+        return $this;
+    }
+
+    /**
+     * Get dateDeSortie
+     *
+     * @return \DateTime
+     */
+    public function getDateDeSortie()
+    {
+        return $this->dateDeSortie;
+    }
+
+    /**
+     * Set realisateur
+     *
+     * @param string $realisateur
+     *
+     * @return Film
+     */
+    public function setRealisateur($realisateur)
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get realisateur
+     *
+     * @return string
+     */
+    public function getRealisateur()
+    {
+        return $this->realisateur;
+    }
+
+    /**
+     * Set genre
+     *
+     * @param string $genre
+     *
+     * @return Film
+     */
+    public function setGenre($genre)
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * Get genre
+     *
+     * @return string
+     */
+    public function getGenre()
+    {
+        return $this->genre;
+    }
+}
+
